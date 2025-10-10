f4b53140d551db4cd71a1427cece773178aef9b4
Lulika57 <kharchenko.anastasya2013@gmail.com>
2025-10-09 20:30:54 -0500

    updating to section

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ function App() {
   return (
     <main>
       <header className="text-center">
-      <h1 className="text-[1.25rem] text-[#f9f4da] font-medium">Assembly: Endgame</h1>
-      <p className="text-[0.875rem] text-[#8E8E8E] max-w-[350px]">Guess the word within 10 attempts to keep the 
-        programming world safe from Assembly!
-      </p>
+        <h1 className="text-[1.25rem] text-[#f9f4da] font-medium">Assembly: Endgame</h1>
+        <p className="text-[0.875rem] text-[#8E8E8E] max-w-[350px]">Guess the word within 10 attempts to keep the programming world safe from Assembly!</p>
       </header>
-      <div>You won!</div>
-      <div>You lost!</div>
+      <section >
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
     </main>
   )
 }
